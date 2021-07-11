6bb802982de86d6076f1f1beefcbd0a0aafd07de
jnst <jnst@outlook.com>
2021-07-11 16:03:12 +0900

    Add introduction

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod hello_world;
+mod introduction;
 mod leet_code;
 
 extern crate clap;
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
 mod hello_world;
+mod introduction;
 use clap::{App, SubCommand};
 
 fn main() {
-    let matches = App::new("Sample code")
+    let matches = App::new("The Rust Programming Language")
         .version("0.1")
         .author("jnst <jnst@outlook.com>")
         .about("Run sample code")
         .subcommand(SubCommand::with_name("hello"))
-            .about("Run hello world")
+            .about("Run hello_world.rs")
+        .subcommand(SubCommand::with_name("introduction"))
+            .about("Run introduction.rs")
         .get_matches();
 
     match matches.subcommand() {
         ("hello", Some(_matches)) => {
-            crate::hello_world::hello();
+            hello_world::hello();
         }
-        _ => unreachable!(),
+        ("introduction", Some(_matches)) => {
+            let rect = introduction::Rectangle { width: 30, height: 25 };
+            println!("{:#?}", rect);
+            println!("The area of the rectangle is {} square pixels.", rect.area())
+        }
+        _ => {
+            unreachable!();
+        },
     }
 }
