28de41251c244baf643ec5f479bfc186f27c6432
jnst <jnst@outlook.com>
2021-07-15 15:33:04 +0900

    Add file operation

--- src/introduction.rs
+++ src/introduction.rs
@@ -1,3 +1,8 @@
+use std::{
+    fs::File,
+    io::{ErrorKind, Read},
+};
+
 #[derive(Debug)]
 pub struct Rectangle {
     pub width: u32,
@@ -8,4 +13,24 @@ impl Rectangle {
     pub fn area(&self) -> u32 {
         self.width * self.height
     }
+}
+
+pub fn print_or_create_file(path: &str) {
+    let f = File::open(path);
+    let mut f = match f {
+        Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            match File::create(path) {
+                Ok(fc) => fc,
+                Err(e) => panic!("Failed to create file: {:?}", e),
+            }
+        },
+        Err(error) => {
+            panic!("Failed to open file: {:?}", error)
+        },
+    };
+
+    let mut buf = String::new();
+    let _ = f.read_to_string(&mut buf);
+    println!("{}", buf);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 mod hello_world;
 mod introduction;
-use clap::{App, SubCommand};
+use clap::{App, Arg, SubCommand};
 
 fn main() {
     let matches = App::new("The Rust Programming Language")
         .version("0.1")
         .author("jnst <jnst@outlook.com>")
         .about("Run sample code")
-        .subcommand(SubCommand::with_name("hello"))
-            .about("Run hello_world.rs")
-        .subcommand(SubCommand::with_name("introduction"))
+        .subcommand(SubCommand::with_name("hello")
+            .about("Run hello_world.rs"))
+        .subcommand(SubCommand::with_name("introduction")
             .about("Run introduction.rs")
+            .arg(Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .value_name("FILE")
+                .takes_value(true)
+                .help("Specify filename")))
         .get_matches();
 
     match matches.subcommand() {
@@ -18,9 +24,14 @@ fn main() {
             hello_world::hello();
         }
         ("introduction", Some(_matches)) => {
-            let rect = introduction::Rectangle { width: 30, height: 25 };
-            println!("{:#?}", rect);
-            println!("The area of the rectangle is {} square pixels.", rect.area())
+            if _matches.is_present("file") {
+                let filename = _matches.value_of("file").unwrap();
+                introduction::print_or_create_file(filename);
+            } else {
+                let rect = introduction::Rectangle { width: 30, height: 25 };
+                println!("{:#?}", rect);
+                println!("The area of the rectangle is {} square pixels.", rect.area())
+            }
         }
         _ => {
             unreachable!();
