3c9e98dc7ce26c950d5c3bd7416986848a43a5bb
jnst <jnst@outlook.com>
2023-06-27 19:52:53 +0900

    Fix the unnecessary access to indices

--- src/leet_code/two_sum.rs
+++ src/leet_code/two_sum.rs
@@ -2,7 +2,7 @@ use std::vec::Vec;
 
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     for i in 0..nums.len() {
-        for j in 1..nums.len() {
+        for j in i + 1..nums.len() {
             if nums[i] + nums[j] == target {
                 return vec![i as i32, j as i32];
             }
